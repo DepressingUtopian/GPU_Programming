40bf21d006fc347fb4361ca12c9b73e8af5d48be
DepressingUtopian <DepressingUtopian@gmail.com>
2019-03-16 21:20:07 +0700

    HotFix LAB 3
    Добавлены алгоритмы инициализации матрицы и транспонирования на  GPU.

--- LAB_Three/kernel.cu
+++ LAB_Three/kernel.cu
@@ -6,6 +6,11 @@
 #include <clocale>
 #include <stdio.h>
 
+#include <cuda_occupancy.h>
+
+
+
+//const int arraySize = 30484848;
 //Макрос для обработки ошибок
 #define cudaCheckError() {                                          \
  cudaError_t e=cudaGetLastError();                                 \
@@ -14,12 +19,41 @@
    exit(0); \
  }                                                                 \
 }
-//Константы
 
-const int countThreads = 1024; //Кол во Нитей
-const int countBlocks = 1;
+__global__ void MatrixInicialization(float *_matrix)
+{
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
+	int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+	int N = blockDim.x * gridDim.x;
+
+	_matrix[i + j * N] = (float)(i + j * N);
+}
+
+__global__ void MatrixInicializationBlock(float *_matrix)
+{
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
+	int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+	int N = blockDim.x * gridDim.x;
+
+	_matrix[i + j * N] = (float)(i + j * N);
+}
+					
+__global__ void MatrixTranspose(float *_matrix1, float *_matrix2)
+{
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
+	int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+	int N = blockDim.x * gridDim.x;
+
+	_matrix1[j + i * N] = _matrix2[i + j * N];
+}
 
 void getViewGPUSpec();
+void Occupancy(int N);
+void ViewMatrix(float *a, int size);
+void MatrixTest(int size,void* func);
 
 using namespace std;
 
@@ -27,6 +61,13 @@ int main()
 {
 	setlocale(LC_ALL,"Russian");
 	getViewGPUSpec();
+
+	for (long long int i = 10; i < 10e4; i <<= 1)
+	{
+		Occupancy(i);
+	}
+	MatrixTest(20, MatrixInicialization);
+
     return 0;
 }
 
@@ -57,9 +98,242 @@ void getViewGPUSpec()
 			cout << "Количество нитей в варпе  : " << prop.warpSize << endl;
 			cout << "Количество мультипроцессоров  : " << prop.multiProcessorCount << endl;
 			cout << "Объем константной памяти  : " << prop.totalConstMem << endl;
-			cout << "Максимальное количество мультипроцессоров на нить : " << prop.maxThreadsPerMultiProcessor << endl;
-			cout << "Максимальное количество потоков на блок  : " << prop.maxThreadsPerBlock << endl;
+			cout << "Максимальное количество нитей на  мультипроцессор : " << prop.maxThreadsPerMultiProcessor << endl;
+			cout << "Максимальное количество нитей на блок  : " << prop.maxThreadsPerBlock << endl;
+
+			cout <<endl << "Теоретическая заполняемость о числа нитей в блоке" << endl;
+			cout << "Оптимальное количество блоков в варпе :" << prop.maxThreadsPerMultiProcessor / prop.warpSize << endl;
+			cout << "Оптимальное количество нитей в блоке :" << prop.maxThreadsPerMultiProcessor / prop.multiProcessorCount << endl;
+		
 
 		}
 }
 
+void Occupancy(int N)
+{
+	int blockSize = 0;
+	int numBlocks = 0;
+	int minGridSize = 0;
+	int activeWarps = 0;
+	int maxWarps = 0;
+
+	int arraySize = N;
+	int device;
+
+	int gridSize;
+
+	float *a = new float[arraySize];
+	float *b = new float[arraySize];
+
+	float *dev_a = 0;
+	float *dev_b = 0;
+
+	cudaError_t cudaStatus;
+
+	cudaStatus = cudaSetDevice(0);
+
+	cudaDeviceProp prop;
+
+	cudaGetDevice(&device);
+	cudaGetDeviceProperties(&prop, device);
+
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+
+	}
+	cudaStatus = cudaMalloc((void**)&dev_a, arraySize * sizeof(float));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+
+	}
+
+	cudaStatus = cudaMalloc((void**)&dev_b, arraySize * sizeof(float));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+
+	}
+
+	cudaStatus = cudaMemcpy(dev_a, a, arraySize * sizeof(float), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+
+	}
+
+	cudaStatus = cudaMemcpy(dev_b, b, arraySize * sizeof(float), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+
+	}
+
+
+	cudaOccupancyMaxPotentialBlockSize(
+		&minGridSize,
+		&blockSize,
+		(void*)MatrixInicialization,
+		0,
+		arraySize);
+
+	cudaOccupancyMaxActiveBlocksPerMultiprocessor(
+		&numBlocks,
+		MatrixInicialization,
+		blockSize,
+		0);
+
+
+	// Round up according to array size
+	gridSize = (arraySize + blockSize - 1) / blockSize;
+
+
+
+	//gTest2 <<<countBlocks, countThreads >>> (dev_b);
+	//cudaDeviceSynchronize();
+
+	activeWarps = numBlocks * blockSize / prop.warpSize;
+	maxWarps = prop.maxThreadsPerMultiProcessor / prop.warpSize;
+
+	cout << "Метрика для N -мерного вектора" << endl;
+//	cout << "Оптимальное количество варпов  для N = " << arraySize << " , = " <<  (double)gridSize / ((gridSize + blockSize - 1) / gridSize) << endl;
+	cout << "Оптимальное количество блоков   для N = " << arraySize << " , = " << gridSize << endl;
+	cout << "Оптимальное количество нитей   для N = " << arraySize << " , = " << blockSize << endl;
+	std::cout << "Occupancy: " << (double)activeWarps / maxWarps * 100 << "%" << std::endl;
+	std::cout << "Occupancy: " << (double)((blockSize * prop.multiProcessorCount) / (gridSize * maxWarps)) * 100 << "%" << std::endl;
+	MatrixInicialization <<<gridSize, blockSize >>> (dev_a);
+	cudaDeviceSynchronize();
+
+Error:
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+}
+
+void getOccupancy(int N,int &_gridSize,int &_blockSize, void* func)
+{
+	int blockSize = 0;
+	int numBlocks = 0;
+	int minGridSize = 0;
+	int activeWarps = 0;
+	int maxWarps = 0;
+
+	int arraySize = N;
+	int device;
+
+	int gridSize;
+
+
+	cudaError_t cudaStatus;
+
+
+	cudaOccupancyMaxPotentialBlockSize(
+		&minGridSize,
+		&blockSize,
+		MatrixInicialization,
+		0,
+		N);
+
+	// Round up according to array size
+	gridSize = (N + blockSize - 1) / blockSize;
+
+	_gridSize = gridSize;
+	_blockSize = blockSize;
+}
+void MatrixTest(int size, void* func)
+{
+
+	int blockSize = 0;
+	int gridSize = 0;
+
+	dim3 grid(size, size);
+
+	float *a = new float[size * size];
+	float *b = new float[size * size];
+	float *c = new float[size * size];
+
+
+	float *dev_a = nullptr;
+	float *dev_b = nullptr;
+	float *dev_c = nullptr;
+
+	cudaError_t cudaStatus;
+
+	cudaStatus = cudaMalloc((void**)&dev_a, size * size * sizeof(float));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+
+	}
+
+	cudaStatus = cudaMemcpy(dev_a, a, size * size * sizeof(float), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+
+	}
+
+	cudaStatus = cudaMalloc((void**)&dev_b, size * size * sizeof(float));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+
+	}
+
+	cudaStatus = cudaMemcpy(dev_b, b, size * size * sizeof(float), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+
+	}
+
+	cudaStatus = cudaMalloc((void**)&dev_c, size * size * sizeof(float));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+
+	}
+
+	cudaStatus = cudaMemcpy(dev_c, c, size * size * sizeof(float), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+
+	}
+	
+	getOccupancy(size, gridSize, blockSize, func);
+	
+
+	MatrixInicialization <<<1, grid >>> (dev_a);
+	MatrixInicializationBlock << <grid, 1 >> > (dev_b);
+	MatrixTranspose << <1, grid >> > (dev_c,dev_a);
+	cudaDeviceSynchronize();
+
+	cudaStatus = cudaMemcpy(a,dev_a, size * size  * sizeof(float), cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+
+	}
+	cudaStatus = cudaMemcpy(b, dev_b, size * size * sizeof(float), cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+
+	}
+	cudaStatus = cudaMemcpy(c, dev_c, size * size * sizeof(float), cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+
+	}
+
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+	cudaFree(dev_c);
+
+	ViewMatrix(c,size);
+}
+
+void ViewMatrix(float *a,int size)
+{
+	cout << endl;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+			if((j + i * size) % size == 0)
+				cout << a[j + i * size] << " ";
+			else
+				cout << "\t" << a[j + i * size] << " ";
+
+		cout << endl;
+	}
+				
+	cout << endl;
+}
